947a85c5b264b211646edb1af87e238e74ae496a
v-rana <63035799+v-rana@users.noreply.github.com>
2023-12-16 03:03:39 +0530

    maybe it will work

--- Patent_Database/wsgi.py
+++ Patent_Database/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 import sys
 from django.core.wsgi import get_wsgi_application
-sys.path.append("Patent_Database\Patent_Database")
+sys.path.append("Patent_Database")
 #
 os.environ['DJANGO_SETTINGS_MODULE'] = 'Patent_Database.settings'
 application = get_wsgi_application()
