6ee6a49c9d75f6db2b1fec0412b58d91454b866e
v-rana <63035799+v-rana@users.noreply.github.com>
2023-12-17 16:45:26 +0530

    fixed Home page anchor try4

--- Main_Functions/urls.py
+++ Main_Functions/urls.py
@@ -6,6 +6,5 @@ from . import views
 urlpatterns = [
     path('patent_list/', views.patent_list, name='patent_list'),
     path('',views.base,name='base'),
-    path('home/',views.base,name='base'),
     path('search/',views.search,name='search')# Add other URLs as needed
 ]
